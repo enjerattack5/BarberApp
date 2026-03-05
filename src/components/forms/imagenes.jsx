c238ec6c9378d28a354fdd41ab72b38d32593351
Beet493 <julizandro11@gmail.com>
2026-03-04 19:00:11 -0600

    mensoimagenes
    Updated the layout and styling of the Perfil component, including changes to image sizes and button placements.

--- src/components/forms/imagenes.jsx
+++ src/components/forms/imagenes.jsx
@@ -9,24 +9,39 @@ export function Perfil() {
       />
       <img
         src="https://i.pinimg.com/originals/59/c8/5b/59c85bf82ccab6e1bbc3fae00ef8c356.jpg"
-        className="w-1/2 mt-6 rounded-xl shadow-lg"
+        className="w-2/3 mt-6 rounded-xl"
         alt="Barbería"
       />
-      <h3 className="mt-8 text-2xl font-bold">Barberos</h3>
-            <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-white rounded-xl shadow-md mt-4 w-1/2">
+      <h3 className="mt-8 text-2xl font-bold">Barberos</h3>   
+            <div className=" sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-white rounded-xl ">
+        
+        <div className=" flex gap-13 overflow-x-auto">
         <img
           className="mx-auto block h-24 w-24 rounded-full sm:mx-0 sm:shrink-0 object-cover"
           src="https://img.freepik.com/fotos-premium/primer-plano-fotografia-hombre-elegante-posando-traje-tradicional_999340-16017.jpg"
           alt="Barbero"
         />
+        <img
+        className="mx-auto block h-24 w-24 rounded-full sm:mx-0 sm:shrink-0 object-cover"
+          src="https://img.freepik.com/fotos-premium/primer-plano-fotografia-hombre-elegante-posando-traje-tradicional_999340-16017.jpg"
+          alt="Barbero"
+        />
+        </div>
         <div className="space-y-2 text-center sm:text-left">
-          <div>
+          <div className=" flex gap-10 overflow-x-auto">
             <h3 className="text-lg font-semibold text-black">Erin Lindford</h3>
-            <h3 className="font-medium text-gray-500">Barbero</h3>
+            
+            <h3 className="text-lg font-semibold text-black">Pulpo Tigre</h3>
+          
           </div>
-          <button className="px-4 py-2 rounded-lg border border-purple-200 text-black-600 hover:bg-green-600 hover:text-white transition">
-            Whatsapp
+           <div className=" flex gap-10 overflow-x-auto">
+          <button className=" px-4 py-2 gap-30 rounded-lg border border-purple-200 text-black-600 hover:bg-green-600 hover:text-white transition ">
+            Whatsapp 
               </button>
+              <button className="px-4 py-2 rounded-lg border border-purple-200 text-black-600 hover:bg-green-600 hover:text-white transition">
+            Whatsapp 
+              </button>
+              </div>
         </div>
       </div>
 
