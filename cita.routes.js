4e185b477e4e48e05a06a3e71401d2634e3d2e2a
Giovani-jefe_Grupo <enjerattack5@gmail.com>
2026-03-31 20:08:05 -0600

    Create cita.routes.js

---
+++ cita.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const CitaController = require("../controllers/cita.controller");
+
+const api = express.Router();
+
+api.post("/citas/crear", CitaController.crearCita);
+api.get("/citas/buscar", CitaController.obtenerCitas);
+
+module.exports = api;
